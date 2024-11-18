d95a9730be51c6cb13315f22a1cc46d8f91750b5
Ayyadurai <kmayyadurai286@gmail.com>
2024-11-18 21:20:45 +0530

    Module 4 finished

--- module 1/postapp/posts/urls.py
+++ module 1/postapp/posts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from posts.views import get_hello_world,get_hello_world2
+from posts.views import get_hello_world, get_post
 
 urlpatterns = [
-    path("get-hello-world/",get_hello_world), # RUN THIS
-    path("get-hello-world2/",get_hello_world2),
+    path("get-hello-world/", get_hello_world),  # RUN THIS
+    path("get/", get_post),
 ]
--- module 1/postapp/posts/views.py
+++ module 1/postapp/posts/views.py
@@ -1,9 +1,7 @@
-from django.shortcuts import render
 from django.http.response import JsonResponse
+from posts.models import Post
+
 # Create your views here.
-
-
-""
 
 # C R U D => Create , Read , Update , Delete
 
@@ -17,10 +15,16 @@ def get_hello_world(request):
     }
     return JsonResponse(data)
 
-def get_hello_world2(request):
-    data = {
-        "message": "Hello , World 2",
-        "quote": "All is well 2"
-    }
-    return JsonResponse(data)
 
+# ORM - Object Relational Mapping
+
+def get_post(request):
+    post = Post.objects.get(id=id)  # SELECT * FROM POST WHERE ID = 1;
+    data = {
+        "id": post.id,
+        "title": post.title,
+        "description": post.description,
+        "image": str(post.image)
+    }
+
+    return JsonResponse(data)
