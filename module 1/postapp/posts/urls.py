67c416adc5d6ada2ee42b53c5d4b8eb599a9f575
Ayyadurai <kmayyadurai286@gmail.com>
2024-11-20 07:37:16 +0530

    List and Delete API completed

--- module 1/postapp/posts/urls.py
+++ module 1/postapp/posts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from posts.views import get_hello_world, get_post
+from posts.views import get_hello_world, get_post, list_posts, delete_post
 
 urlpatterns = [
     path("get-hello-world/", get_hello_world),  # RUN THIS
-    path("get/", get_post),
+    path("get/<int:id>/", get_post),  # ONE RECORD
+    path("list/", list_posts),  # MULTIPLE RECORDS
+    path("delete/<int:id>/", delete_post),  # DELETE ONE RECORD
+
 ]
--- module 1/postapp/posts/views.py
+++ module 1/postapp/posts/views.py
@@ -18,8 +18,11 @@ def get_hello_world(request):
 
 # ORM - Object Relational Mapping
 
-def get_post(request):
-    post = Post.objects.get(id=id)  # SELECT * FROM POST WHERE ID = 1;
+def get_post(request, id):
+    try:
+        post = Post.objects.get(id=id)  # SELECT * FROM POST WHERE ID = 1;
+    except Post.DoesNotExist:
+        return JsonResponse({"message": "Post doesn't exists , check ID"})
     data = {
         "id": post.id,
         "title": post.title,
@@ -28,3 +31,15 @@ def get_post(request):
     }
 
     return JsonResponse(data)
+
+
+def list_posts(request):
+    posts = Post.objects.all().values("id", "title", "description", "image") # []
+    data = list(posts)
+    return JsonResponse(data, safe=False)
+
+
+def delete_post(request, id):
+    post = Post.objects.get(id=id)  # SELECT * FROM POST WHERE ID = 1;
+    post.delete()
+    return JsonResponse({"message": f"Post {id} deleted"})
